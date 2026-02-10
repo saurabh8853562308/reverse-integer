012a7a215f0617d3807a8ed46c14f70ddc925b92
saurabh8853562308 <shuklasaurabh6200@gmail.com>
2026-02-10 12:28:00 +0530

    Update integer reverse.cpp

--- integer reverse.cpp
+++ integer reverse.cpp
@@ -1,17 +1,22 @@
-#include <iostream>
-using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
 
-int main() {
-    int n, rev = 0;
-    cin >> n;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
 
-    while (n != 0) {
-        int digit = n % 10;
-        rev = rev * 10 + digit;
-        n = n / 10;
+            // overflow check (32-bit only)
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
+                return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
+                return 0;
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
     }
-
-    cout << rev;
-    return 0;
-}
+};
 
